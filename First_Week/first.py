ca4d744d8c439c762e74e6b20f0e37b8dad09ae2
Madarász Tamás <m.tamas1012@gmail.com>
2021-10-07 23:19:07 +0200

    Initial commit

---
+++ First_Week/first.py
@@ -0,0 +1,8 @@
+print("Your name:")
+name = input()
+
+print("Your age: ")
+age = int(input())
+
+print("Your name: {}, your age: {}".format(name, age))
+print("Next year you will be {} years old".format(age + 1))
